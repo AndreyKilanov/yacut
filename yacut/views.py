150cb6cb62710585951639d7efa4ea8e9a289f84
Андрей Киланов <8941519@mail.ru>
2023-07-31 00:02:09 +0500

    review_7

--- yacut/views.py
+++ yacut/views.py
@@ -17,7 +17,8 @@ def index_view():
     try:
         url_map = URLMap.create_link(form.original_link.data, short_id)
         short_id = URLMap.full_short_id(url_map.short)
-    except flash(INVALID_CREATE_OBJECT):
+    except:
+        flash(INVALID_CREATE_OBJECT)
         return render_template(INDEX_HTML, form=form)
     return render_template(INDEX_HTML, form=form, short_link=short_id)
 
